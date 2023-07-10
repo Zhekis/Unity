ecc9e3e50f7177d4d6767dcdfa95ca234147661f
zh <zh@DESKTOP-D708L2T>
2023-07-10 15:09:51 +0500

    + Enemy, fix Instatiate

--- Assets/Scripts/EnemyInstantiate.cs
+++ Assets/Scripts/EnemyInstantiate.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class EnemyInstantiate : MonoBehaviour
 {
     [SerializeField] private Transform _path;
+    [SerializeField] private Enemy _template;
 
-    public GameObject Template;
     private Transform[] _points;
 
     private void Start()
@@ -22,9 +22,7 @@ public class EnemyInstantiate : MonoBehaviour
         for (int i = 0; i < _path.childCount; i++)
         {
             _points[i] = _path.GetChild(i);
-            GameObject newObject = Instantiate(Template, new Vector3(0, 0, 0), Quaternion.identity);
-            Transform newObjectTransform = newObject.GetComponent<Transform>();
-            newObjectTransform.position = _points[i].position;
+            var spawnedEnemy = Instantiate(_template, _points[i].position, Quaternion.identity);
 
             yield return waitForTwoSeconds;
         }
